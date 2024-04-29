06148c93cfd62cb111667866d9fdaf05ad6d237b
Emiel Steerneman <emielsteerneman@gmail.com>
2024-04-29 14:52:36 +0200

    Added dotenv

--- function_app.py
+++ function_app.py
@@ -1,7 +1,9 @@
 import azure.functions as func
+import dotenv
+from dotenv import load_dotenv
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route('hello')
 def main(req):
-    return func.HttpResponse("Hello World!", mimetype="text/plain")
+    return func.HttpResponse("Hello World! : " + str(load_dotenv), mimetype="text/plain")
