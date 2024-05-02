3dd274482ce0e5592764481249aa1f0f2944861d
Emiel Steerneman <emielsteerneman@gmail.com>
2024-05-02 20:38:18 +0200

    Moved python version down to 3.8

--- function_app.py
+++ function_app.py
@@ -1,9 +1,12 @@
+import sys
 import azure.functions as func
 import dotenv
 from dotenv import load_dotenv
+
+print(sys.version)
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route('hello')
 def main(req):
-    return func.HttpResponse("Hello World! : " + str(load_dotenv), mimetype="text/plain")
+    return func.HttpResponse(f"Hello World! : python={sys.version}, dotenv={str(load_dotenv)}", mimetype="text/plain")
